dd974fea8768cf6c310c506b1381b2dec3a76502
crucis-onishi <crucis@crucis.xsrv.jp>
2018-08-09 21:22:26 +0900

    change

--- demotheme/header.php
+++ demotheme/header.php
@@ -4,7 +4,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
 <?php wp_head(); ?>
 </head>
 
